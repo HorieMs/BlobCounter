bd2c8ff41ae0398fbc3cdf471a99b7c77fb2dc94
HorieMs <horie.ms10@gmail.com>
2022-03-26 08:09:31 +0900

    2nd commit

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ import plotly.graph_objs as go
 # Blobパラメータの初期化
 params = cv2.SimpleBlobDetector_Params() 
 
+# color_enhancement_menu=('None','Hue rotation','Hue extraction')
+# color_enhancement_mode=color_enhancement_menu[0]
+# h_offset=5     # 色相回転角[deg], 0のときは、色相抽出
+# hsv_gain=np.array([1.0,1.0,1.0])
+# h_ext_center=90     # 色相抽出角[deg],0-360
+# h_ext_hrange=90     # 色相抽出角[deg. ] h_ext_center-h_ext_hrange ...　h_ext_center+h_ext_hrange の範囲のみを抽出
+
+
 
 @st.cache
 def convert_df(df):
@@ -45,9 +53,9 @@ def simple_blob(image):
     # ブロブの個数  
     count = len(kp) 
     #print(f'丸の個数: {count}')
-    text=f"{count:>6d} Blobs"
+    text=f"{count:>6d} Blob"
     org=(0,int(blobs.shape[0]*0.98))
-    blobs=cv2.putText(blobs, text, org, fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=5, color=(255,0,0))
+    blobs=cv2.putText(blobs, text, org, fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=4, color=(255,0,0))
 
 
     
@@ -92,11 +100,67 @@ def simple_blob(image):
     st.plotly_chart(fig, use_container_width=True)
 
 
+# def color_enhancement(img):
+#     hsv_img=cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL) # H最大値255で変換した結果
+
+#     enh_hsv_img=np.empty(hsv_img.shape,dtype='uint8')
+
+#     if color_enhancement_mode==color_enhancement_menu[1]:
+#         # 色相回転の場合
+#         if h_offset!=0:
+#             enh_h=np.uint16((np.round((hsv_img[:,:,0]+h_offset)*hsv_gain[0])))%256
+#         enh_s=np.clip(np.round(hsv_gain[1]*hsv_img[:,:,1]),0,255)
+#         enh_v=np.clip(np.round(hsv_gain[2]*hsv_img[:,:,2]),0,255)
+#     else:
+#         # 色相抽出の場合
+#         h_start=np.int16(np.round(255*(h_ext_center-h_ext_hrange)/360))
+#         h_end=np.int16(np.round(255*(h_ext_center+h_ext_hrange)/360))
+#         enh_h=hsv_img[:,:,0]
+#         enh_s=hsv_img[:,:,1]
+#         enh_v=hsv_img[:,:,2]
+#         if (h_start>=0) and (h_end<=255):
+#             bin_img=(hsv_img[:,:,0]<h_start) | (hsv_img[:,:,0]>h_end)
+#             enh_v[bin_img]=0
+#         elif (h_start<0) and (h_end<=255):
+#             h_start=255+h_start
+#             bin_img=(hsv_img[:,:,0]>h_end) & (hsv_img[:,:,0]<h_start)
+#             enh_v[bin_img]=0
+#         elif (h_start>=0) and (h_end>255):
+#             h_end=(h_end % 256)
+#             bin_img=(hsv_img[:,:,0]>h_end) & (hsv_img[:,:,0]<h_start)
+#             enh_v[bin_img]=0
+#         else:
+#             pass
+
+#         enh_s=np.clip(np.round(hsv_gain[1]*enh_s),0,255)
+#         enh_v=np.clip(np.round(hsv_gain[2]*enh_v),0,255)
+
+#     enh_hsv_img[:,:,0]=np.uint8(enh_h)
+#     enh_hsv_img[:,:,1]=np.uint8(enh_s)
+#     enh_hsv_img[:,:,2]=np.uint8(enh_v)
+
+#     enh_bgr_img=cv2.cvtColor(enh_hsv_img, cv2.COLOR_HSV2BGR_FULL) # H最大値255で変換した結果
+#     return enh_bgr_img
+
+
 
 def disp_sidebar():
-    st.sidebar.header('Blob parameters')
 
-    chk_fbc=st.sidebar.checkbox('Filter by Color',key='chk_fbc')
+    st.sidebar.header('Parameters')
+    # st.sidebar.subheader('Color enhancement')
+
+    # if color_enhancement_mode==color_enhancement_menu[0]:
+    #     index=0
+    # elif color_enhancement_mode==color_enhancement_menu[1]:
+    #     index=1
+    # else:
+    #     index=2
+    
+    # color_enhancement_mode=st.sidebar.selectbox('Mode', options=color_enhancement_menu)
+
+    st.sidebar.header('Blob')
+
+    chk_fbc=st.sidebar.checkbox('Filter by Color',value=params.filterByColor,key='chk_fbc')
     if chk_fbc:
         params.filterByColor=True
         BlobType = st.sidebar.radio( "Blob type",     ('Dark', 'Light'),key='BlobType')
@@ -108,7 +172,7 @@ def disp_sidebar():
         params.filterByColor=False
     
     # ブロブ領域（minArea <= blob < maxArea）
-    chk_fba=st.sidebar.checkbox('Filter by Area',key='chk_fba')
+    chk_fba=st.sidebar.checkbox('Filter by Area',value=params.filterByArea,key='chk_fba')
     if chk_fba:
         params.filterByArea = True
         params.minArea=st.sidebar.number_input('MinArea',min_value=0,max_value=1000,value=int(params.minArea),step=1,format='%d',key='fba_min')
@@ -118,7 +182,7 @@ def disp_sidebar():
 
     # 真円度（ 4∗π∗Area / perimeter∗perimeter によって定義される）
     #（minCircularity <= blob < maxCircularity）
-    chk_fbci=st.sidebar.checkbox('Filter by Circularity',key='chk_fbci')
+    chk_fbci=st.sidebar.checkbox('Filter by Circularity',value=params.filterByCircularity,key='chk_fbci')
     if chk_fbci:
         params.filterByCircularity = True
         params.minCircularity=max(0.0,params.minCircularity)
@@ -131,7 +195,7 @@ def disp_sidebar():
 
     
     # 凸面の情報（minConvexity <= blob < maxConvexity）
-    chk_fbco=st.sidebar.checkbox('Filter by Convexity',key='chk_fbco')
+    chk_fbco=st.sidebar.checkbox('Filter by Convexity',value=params.filterByConvexity,key='chk_fbco')
     if chk_fbco:
         params.filterByConvexity = True
         params.minConvexity=max(0.0,params.minConvexity)
@@ -143,7 +207,7 @@ def disp_sidebar():
         params.filterByConvexity = False
 
     # 楕円形を表す（minInertiaRatio <= blob < maxInertiaRatio）
-    chk_fbi=st.sidebar.checkbox('Filter by InertiaRatio',key='chk_fbi')
+    chk_fbi=st.sidebar.checkbox('Filter by InertiaRatio',value=params.filterByInertia,key='chk_fbi')
     if chk_fbi:
         params.filterByInertia = True
         params.minInertiaRatio=max(0.0,params.minInertiaRatio)
